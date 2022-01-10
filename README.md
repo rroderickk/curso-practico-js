ccc1cd59395b85b7a9dfdd5886cd1a3f40c0689a
RodrigoMoreno <rroderickk@gmail.com>
2022-01-09 19:29:09 -0500

    graficos listo el esquema html

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 ## Taller $1: Figuras Geometricas
     -1: Definir las formulas.
-    -2: Implementar las formulas en Js
-    -3: Crear funciones
-    -4: Integrar JS con HTML
+    -2: Implementar las formulas en Js.
+    -3: Crear funciones.
+    -4: Integrar JS con HTML.
+
+## Taller $2: Porcentajes 
+    -1: Definir las formulas.
+    -2: Implementar las formulas.
+    -3: Crear funciones.
+    -4: Integrar JS con HTML.
--- figuras.js
+++ figuras.js
@@ -1,3 +1,4 @@
+/*
 //? Codigo del Cuadrado.
 console.group('Cuadrado');
 function CalculaPerimetroDelCuadrado(l) {
@@ -9,16 +10,13 @@ function CalculaPerimetroDelCuadrado(l) {
 CalculaPerimetroDelCuadrado(10);
 console.groupEnd('Cuadrado');
 
-
 //? Codigo del Triangulo Lite.
-/* console.group('Triangulo')
+console.group('Triangulo')
 function CalculaPerimetroDelTriangulo(base,altura) {
     return PerimetroDelTriangulo;
 }
 CalculaPerimetroDelTriangulo();
 console.groupEnd('Triangulo');
-
- */
 
 //? Codigo del Circulo.
 console.group('Circulo')
@@ -32,18 +30,45 @@ function CalculaPerimetroDelCirculo(Radio) {
 }
 CalculaPerimetroDelCirculo(10);
 console.groupEnd('Circulo');
+*/
 
 //todo Codigo del Renderizado cuadrado.
 function resultCuadrado(e) {
-    result = CalculaPerimetroDelCuadrado(e)
-    labelResult.innerHTML = `${result} cm`; 
+    labelResult.innerHTML = `${+e*4} cm`; 
     labelResult2.innerHTML = `${+e*+e} cm^2`; 
-}
+};
 
-//todo Codigo del Renderizado triangulo.
+//todo Codigo del Renderizado triangulo lite.
 function resultTriangulo(base,altura) {
     labelResultTA.innerHTML =`${(+base * +altura)/2} cm^2`; 
-}
+};
+
+//todo Codigo del Renderizado triangulo isosceles.
+function isosceles(l1, l2, l3) {
+
+    function resultTrianguloIsosceles(a,b,c) {
+        let ab = a-b;
+        let ac = a-c;
+        let $Isosceles = ab == ac ? true : false;
+        labelResultI.innerHTML =`${$Isosceles}`; 
+    }
+
+    function altura(base, lado) {
+        console.log({base, lado})
+        let altura = Math.sqrt(Math.pow(base/2,2) + Math.pow(lado,2))
+        console.log(altura.toFixed(2))
+        return altura.toFixed(2);
+    }
+
+    if ((l1 === l2 && l1 != l3) || (l1 == l3 && l1 != l2) || (l2 == l3 && l1 != l3)){
+        l1 === l2 
+            ? labelResultAT.innerHTML =`${altura(l3,l1)} cm` 
+            : l1 === l3 
+                ? labelResultAT.innerHTML =`${altura(l2, l1)}` 
+                : labelResultAT.innerHTML =`${altura(l1, l3)}`
+    }
+    return resultTrianguloIsosceles(l1,l2,l3);
+};
 
 //todo Codigo del Renderizado circulo.
 function resultCirculo(Radio) {
@@ -53,4 +78,5 @@ function resultCirculo(Radio) {
     labelResultDC.innerHTML =`${Radio*2} cm`; 
     labelResultDA.innerHTML =`${Area.toFixed(2)} cm^2`; 
     labelResultP.innerHTML =`${Perimetro.toFixed(2)} cm`; 
-}
+};
+
