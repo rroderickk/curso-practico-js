9dee4a1cf89bb1352f3df59cdb2b4e31e0eab1fd
RodrigoMoreno <rroderickk@gmail.com>
2022-01-13 20:00:46 -0500

    parte3 no resuelta

--- productOffert/main.js
+++ productOffert/main.js
@@ -161,11 +161,11 @@ function resultPriceFinalMax() {
 //**╦═╗╔═╗╔╦╗╔═╗┌─┐┬ ┬┌┐┌┌─┐┬┌─┐┌┐┌┌─┐┌─┐┌┬┐┌─┐┌─┐┬┌┐┌┌┬┐┌─┐┬  ┬┌─┐┌─┐┌┐┌┌┬┐┌─┐┌─┐
 //**╠╦╝║╣  ║ ║ ║├┤ │ │││││  ││ ││││├┤ └─┐│││├─┤└─┐││││ │ ├┤ │  ││ ┬├┤ │││ │ ├┤ └─┐
 //**╩╚═╚═╝ ╩ ╚═╝└  └─┘┘└┘└─┘┴└─┘┘└┘└─┘└─┘┴ ┴┴ ┴└─┘┴┘└┘ ┴ └─┘┴─┘┴└─┘└─┘┘└┘ ┴ └─┘└─┘
-const coupons = [
-    "JuanDC_es_Batman",
-    "pero_no_le_digas_a_nadie",
-    "es_un_secreto",
-];
+// const coupons = [
+//     "JuanDC_es_Batman",
+//     "pero_no_le_digas_a_nadie",
+//     "es_un_secreto",
+// ];
 
 function onClickButtonPriceDiscount() {
 	const inputPrice = document.getElementById("InputCoupon");
@@ -176,19 +176,137 @@ function onClickButtonPriceDiscount() {
 
 	switch(couponValue) {
 		case coupons[0]: // "JuanDC_es_Batman"
-		ethPrice$ = ethPrice.innerHTML = `Felicidades Haz Ganado `+'<br/>'+`${array[2]}`;
-		return ethPrice$;
-		break;
+			ethPrice$ = ethPrice.innerHTML = `Felicidades Haz Ganado `+'<br/>'+`${array[2]}`;
+// 			return ethPrice$;
+// 		break;
 		case coupons[1]: // "pero_no_le_digas_a_nadie"
-		descuento = 30;
+			ethPrice$ = ethPrice.innerHTML = `Felicidades Haz Ganado `+'<br/>'+`Un Flamante Lamborghini UltimaGeneracion`;
+			return ethPrice$;
 		break;
 		case coupons[2]: // "es_un_secreto"
-		descuento = 25;
+			ethPrice$ = ethPrice.innerHTML = `Felicidades Haz Ganado `+'<br/>'+`https://www.mdzol.com/u/fotografias/m/2020/9/25/f848x477-962473_1020276_5050.jpg`;
+			return ethPrice$;
 		break;
 	}
 
 }
 
+//**╦═╗╔═╗╔╦╗╔═╗┌─┐┬ ┬┌┐┌┌─┐┬┌─┐┌┐┌┌─┐┌─┐┌┬┐┌─┐┌─┐┬┌┐┌┌┬┐┌─┐┬  ┬┌─┐┌─┐┌┐┌┌┬┐┌─┐┌─┐
+//**╠╦╝║╣  ║ ║ ║├┤ │ │││││  ││ ││││├┤ └─┐│││├─┤└─┐││││ │ ├┤ │  ││ ┬├┤ │││ │ ├┤ └─┐
+//**╩╚═╚═╝ ╩ ╚═╝└  └─┘┘└┘└─┘┴└─┘┘└┘└─┘└─┘┴ ┴┴ ┴└─┘┴┘└┘ ┴ └─┘┴─┘┴└─┘└─┘┘└┘ ┴ └─┘└─┘
+//!┌─┐┌─┐┬─┐┌┬┐╔╦╗╔═╗╔═╗
+//!├─┘├─┤├┬┘ │  ║║║ ║╚═╗
+//!┴  ┴ ┴┴└─ ┴ ═╩╝╚═╝╚═╝
+/*
+Legibilidad es la facilidad que ofrece nuestro código para entenderlo sin tropiezos cuando lo leemos. Mejorar la legibilidad es mucho más importante que reducir la cantidad de líneas de código.
+
+Además, error-first es un patrón de programación donde atrapamos los errores antes de escribir el resto de la lógica de nuestros programas. Aplicar error-first nos puede ayudar a mejorar la legibilidad de nuestro código (es completamente válido tener otra opinión, no te preocupes, cuéntame en los comentarios).
+
+Error first con array.includes
+La función array.includes() nos permite validar si un elemento ha sido almacenado en el array. Vamos a usar esta función para ejecutar un error si el cupón de los usuarios es inválido (no fue guardado en el array coupons):
+*/
+
+// function onClickButtonPriceDiscountPartDos() {
+// 	const inputPrice = document.getElementById("InputCoupon");
+// 	const priceValue = inputPrice.value;
+	
+// 	const inputCoupon = document.getElementById("InputCoupon");
+// 	const couponValue = inputCoupon.value;
+
+// 	if (!coupons.includes(couponValue)) {
+// 		alert("El cupón " + couponValue + "no es válido");
+// 	}
+
+// // Ya que estamos usando un condicional if, podemos reescribir el resto del condicional switch, pero en este caso usaremos condicionales else if:
+
+// 	if (!coupons.includes(couponValue)) {
+// 			alert("El cupón " + couponValue + "no es válido");
+// 		} else if (couponValue === "JuanDC_es_Batman") {
+// 			ethPrice$ = ethPrice.innerHTML = `Felicidades Haz Ganado `+'<br/>'+`${array[2]}`;
+// 		} else if (couponValue === "pero_no_le_digas_a_nadie") {
+// 			ethPrice$ = ethPrice.innerHTML = `Felicidades Haz Ganado `+'<br/>'+`1000 Bitcoins`;
+// 		} else if (couponValue === "es_un_secreto") {
+// 			ethPrice$ = ethPrice.innerHTML = `Felicidades Haz Ganado `+'<br/>'+`https://www.redfin.com/CA/Los-Angeles/8201-Bellgave-Pl-90069/home/7119460`;
+// 		// descuento = 25;
+// 	}
+// }
+
+//!┌─┐┌─┐┬─┐┌┬┐╔╦╗╔═╗╔═╗
+//!├─┘├─┤├┬┘ │  ║║║ ║╚═╗
+//!┴  ┴ ┴┴└─ ┴ ═╩╝╚═╝╚═╝
+//**╦═╗╔═╗╔╦╗╔═╗┌─┐┬ ┬┌┐┌┌─┐┬┌─┐┌┐┌┌─┐┌─┐┌┬┐┌─┐┌─┐┬┌┐┌┌┬┐┌─┐┬  ┬┌─┐┌─┐┌┐┌┌┬┐┌─┐┌─┐
+//**╠╦╝║╣  ║ ║ ║├┤ │ │││││  ││ ││││├┤ └─┐│││├─┤└─┐││││ │ ├┤ │  ││ ┬├┤ │││ │ ├┤ └─┐
+//**╩╚═╚═╝ ╩ ╚═╝└  └─┘┘└┘└─┘┴└─┘┘└┘└─┘└─┘┴ ┴┴ ┴└─┘┴┘└┘ ┴ └─┘┴─┘┴└─┘└─┘┘└┘ ┴ └─┘└─┘
+//todo╔═╗╔═╗╦═╗╔╦╗╔═╗╔╦╗╦═╗╔═╗╔═╗
+//todo╠═╝╠═╣╠╦╝ ║ ║╣  ║ ╠╦╝║╣ ╚═╗
+//todo╩  ╩ ╩╩╚═ ╩ ╚═╝ ╩ ╩╚═╚═╝╚═╝
+/* //* Solución #3: arrays y condicionales mucho más inteligentes
+Ya usamos uno de los muchos métodos de los arrays para aplicar error first. Pero podemos usar algunos más para simplificar aún más nuestro código.
+
+//! Recuerda que mejorar la legibilidad no (necesariamente) significa reducir líneas de código.
+
+Array de cupones 2.0: ahora también con los descuentos
+Por ahora tenemos muy separada la lógica de nuestros cupones. Por un lado tenemos la lista de cupones, pero en otro lugar distinto definimos el descuento de cada cupón.
+
+Vamos a cambiar eso convirtiendo nuestro array de strings en un array de objetos (con el nombre y descuento de cada cupón en el mismo lugar):
+*/
+const coupons = [
+	{
+		name: "JuanDC_es_Batman",
+		ethPrice$: ethPrice.innerHTML = `Felicidades Haz Ganado `+'<br/>'+`1000 Bitcoins`,
+	},
+	{
+		name: "pero_no_le_digas_a_nadie",
+		ethPrice$: ethPrice.innerHTML = `Felicidades Haz Ganado `+'<br/>'+`100 Bitcoins`,
+	},
+	{
+		name: "es_un_secreto",
+		ethPrice$: ethPrice.innerHTML = `Felicidades Haz Ganado `+'<br/>'+`10 Bitcoins`,
+	},
+];
+/*
+Ahora la función array.includes dejó de funcionar, pero podemos reemplazarla por la función array.find() para validar si el cupón que escribió el usuario está guardado en la propiedad name de alguno de los objetos en la lista de cupones.
+
+Vamos a crear una variable isUserCouponValueValid con una función que valide si su parámetro coupon es igual a nuestra variable userCouponValue. Y esta variable la vamos a enviar como parámetro a nuestra función array.find().
+
+*/
+// const inputPrice = document.getElementById("change").id;
+
+// const priceValue = document.getElementById("InputCoupon").value;
+
+const inputCoupon = document.getElementById("InputCoupon");
+const couponValue = inputCoupon.value;
+
+const userCouponValue = inputCoupon.value;
+
+
+/*
+¡Muy bien!
+
+Pero fíjate que ya tenemos nuestro cupón (con nombre y descuento) disponible en la nueva variable userCoupon.
+
+Podemos aprovechar esta variable para eliminar todos nuestros condicionales else if y cambiarlos por un condicional else que nos proporcione el porcentaje de descuento automáticamente, sin importar el cupón (válido) que estén usando nuestros usuarios.
+*/
+function control(){
+	const isCouponValueValid = function (coupons) {
+		return coupons.name === couponValue;
+	};
+
+	if (!userCouponValue) {
+		alert("El cupón " + couponValue + " no es válido");
+	} else {
+		const userCouponValue = coupons.find(couponValue);
+	}
+	return isCouponValueValid();
+}
+	
+/*
+De esta forma, no solo logramos reducir las líneas de código de nuestro programa, sino también mejorar su legibilidad.
+¡Te espero en la siguiente clase para que iniciemos nuestro tercer taller práctico del curso! 
+*/
+//todo╔═╗╔═╗╦═╗╔╦╗╔═╗╔╦╗╦═╗╔═╗╔═╗
+//todo╠═╝╠═╣╠╦╝ ║ ║╣  ║ ╠╦╝║╣ ╚═╗
+//todo╩  ╩ ╩╩╚═ ╩ ╚═╝ ╩ ╩╚═╚═╝╚═╝
 //**╦═╗╔═╗╔╦╗╔═╗┌─┐┬ ┬┌┐┌┌─┐┬┌─┐┌┐┌┌─┐┌─┐┌┬┐┌─┐┌─┐┬┌┐┌┌┬┐┌─┐┬  ┬┌─┐┌─┐┌┐┌┌┬┐┌─┐┌─┐
 //**╠╦╝║╣  ║ ║ ║├┤ │ │││││  ││ ││││├┤ └─┐│││├─┤└─┐││││ │ ├┤ │  ││ ┬├┤ │││ │ ├┤ └─┐
 //**╩╚═╚═╝ ╩ ╚═╝└  └─┘┘└┘└─┘┴└─┘┘└┘└─┘└─┘┴ ┴┴ ┴└─┘┴┘└┘ ┴ └─┘┴─┘┴└─┘└─┘┘└┘ ┴ └─┘└─┘
